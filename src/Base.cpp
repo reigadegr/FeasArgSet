94659fc656c7f568c86143c70fe5b294a96879cb
reigadegr <2722688642@qq.com>
2023-07-08 21:36:53 +0800

    小幅度优化

--- src/Base.cpp
+++ src/Base.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include "include/function.h"
-
+//#include "include/GameList.h"
+//把第一个节点的值写到另一个节点
 bool Get1To2(std::string input,std::string output)
 {
     std::ifstream input_path(input);
@@ -19,10 +21,41 @@ bool Get1To2(std::string input,std::string output)
     return true;
     
 }
-
+//获取第二个参数
 void GetSecondArg(std::string &buf,std::string& secondArg){
     std::istringstream iss(buf);
     std::string tmp;
     iss >> tmp >> secondArg;
     //LOG(tmp," : ",secondArg);
+}
+
+bool matching_start(std::vector<listGame> gamesaver, std::string &middle_big_core_in_game,FeasPath &feaspath,std::string &gov)
+{
+    std::string FgApp = getTopApp();
+    LOG("前台包名: ",FgApp);
+    int tmp_i = 0;
+    //打印包名
+    for (const auto& game : gamesaver)
+    {        
+        if(FgApp == game.name){
+            LOG("成功匹配");
+            //成功后把结构体内各个对象的值写入到指定路径
+            //...
+            Feas_on(game.fixed_target_fps,game.scaling_a,game.scaling_b,feaspath.Feas_switch,feaspath.Fps,feaspath.scaling_a, feaspath.scaling_b);
+            
+            set_middle_big_gov(middle_big_core_in_game);
+            tmp_i = 1;
+            break;
+        }
+        else{
+            continue;
+        }
+    }
+    
+    if(!tmp_i){
+        Feas_off(feaspath.Feas_switch,feaspath.Fps,feaspath.scaling_a,feaspath.scaling_b);
+        recover_freq();
+        init_gov(gov);
+    }
+    return true;
 }
--- src/Profile.cpp
+++ src/Profile.cpp
@@ -35,7 +35,7 @@ bool readProfile(const char *profile, std::vector<listGame> &profit,std::string 
             {
                 std::istringstream iss(buf);
                 listGame tmpmsg;
-                std::string name = tmpmsg.name = "";
+                std::string name = tmpmsg.name;
                 unsigned int fps = tmpmsg.fixed_target_fps = 60;
                 int scaling_a = tmpmsg.scaling_a = 0;
                 int scaling_b = tmpmsg.scaling_b = 0;
@@ -45,12 +45,15 @@ bool readProfile(const char *profile, std::vector<listGame> &profit,std::string 
                     
                     if(scaling_a > 1000)
                         scaling_a = 1000;
-                    else if(scaling_a < -1000)
+                        
+                    if(scaling_a < -1000)
                         scaling_a = -1000;
+                        
                         
                     if(scaling_b > 1000)
                         scaling_b = 1000;
-                    else if(scaling_b < -1000)
+                        
+                    if(scaling_b < -1000)
                         scaling_b = -1000;
                         
                     profit.push_back({name,fps,scaling_a,scaling_b});
@@ -70,33 +73,27 @@ bool readPathProfile(const char *pathProfile,std::string &Fps_path, std::string 
     std::ifstream file(pathProfile);
     
     if (!file.is_open())
-        return false;        
+        return false;
+            
     std::string buf;
-  
     while (std::getline(file, buf))
     {            
         if(!buf.empty() && buf[0] != '#')
         {
             if(buf.find("Feas_switch_path") != std::string::npos)
-            {
                 GetSecondArg(buf,Feas_switch);
-            }
+            
         
             if(buf.find("Fps_Path") != std::string::npos)
-            {
                 GetSecondArg(buf,Fps_path);
-            }
             
-            if(buf.find("Scaling_a_path") != std::string::npos)
-            {
+            
+            if(buf.find("Scaling_a_path") != std::string::npos)   
                 GetSecondArg(buf,scaling_a);
-            }
+            
             
             if(buf.find("Scaling_b_path") != std::string::npos)
-            {
-                GetSecondArg(buf,scaling_b);
-            }
-            
+                GetSecondArg(buf,scaling_b);         
         }
     }
     return true;
