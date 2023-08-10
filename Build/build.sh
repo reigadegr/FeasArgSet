766189a2e43334da709c8a7381a5248a86b10b41
reigadegr <2722688642@qq.com>
2023-08-10 13:33:01 +0800

    使用shadow3的命令编译降低elf体积

--- Build/build.sh
+++ Build/build.sh
@@ -23,10 +23,11 @@ compile_start(){
     /data/data/com.termux/files/usr/bin/aarch64-linux-android-clang++ \
     -Wall -fomit-frame-pointer -std=c++20 -stdlib=libc++ -Os -flto \
     -fno-rtti -fvisibility=hidden -static-libgcc -static-libstdc++ \
+    -fshort-enums -fmerge-all-constants -fno-exceptions \
     -fuse-ld=lld -mtune=native -march=native -flto -pthread \
-    -Wl,-O3,--lto-O3,--gc-sections,--icf=all,-x,-s \
+    -Wl,-O3,--lto-O3,--gc-sections,--as-needed,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr,-x,-s \
     $(pwd)/*.cpp -o $(dirname "$0")/FeasArgSet && echo "*编译完成*" || exit 1
-    /data/data/com.termux/files/usr/bin/strip $(dirname "$0")/FeasArgSet
+    /data/data/com.termux/files/usr/bin/aarch64-linux-android-strip $(dirname "$0")/FeasArgSet
     chmod +x $(dirname "$0")/FeasArgSet
     cp -f $(dirname "$0")/FeasArgSet  ../Module/FeasArgSet
     mv -f $(dirname "$0")/FeasArgSet  ../Test/FeasArgSet
@@ -38,5 +39,3 @@ echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H)
 format_code
 compile_start
 remove_file 2>/dev/null
-
-
