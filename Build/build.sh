3d98b6c99573f238e3288e29acc12f82f8a00892
shadow3aaa <shadow3aaa@qq.com>
2023-10-09 23:16:17 +0800

    Fix shells

--- Build/build.sh
+++ Build/build.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
-remove_file(){
-    rm -rf $(pwd)/*.bak $(pwd)/include/*.bak $(pwd)/config/*.bak $(dirname "$0")/*.bak
+remove_file() {
+	rm -rf $(pwd)/*.bak $(pwd)/include/*.bak $(pwd)/config/*.bak $(dirname "$0")/*.bak
 }
 
-format_code(){
-    echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
-    code_file="
+format_code() {
+	echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
+	code_file="
         $(dirname "$0")/../src/*.cpp
         $(dirname "$0")/../src/include/*.h
     "
 
-    for i in $code_file; do
-        echo "开始格式化$(basename $i)"
-        /data/data/com.termux/files/usr/bin/clang-format -i $i
-    done
+	for i in $code_file; do
+		echo "开始格式化$(basename $i)"
+		clang-format -i $i
+	done
 }
 
-compile_start(){
-    echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
-    echo "开始编译，大概10秒完成"
-    cd  $(dirname "$0")/../src
-    #this common from user : shadow3aaa
-    /data/data/com.termux/files/usr/bin/aarch64-linux-android-clang++ \
-    -Wall -std=c++2b -stdlib=libc++ -Os -flto \
-    -fno-rtti -fvisibility=hidden -static-libstdc++ \
-    -fshort-enums -fmerge-all-constants -fno-exceptions -fuse-ld=lld \
-    -Bsymbolic -fdata-sections -ffunction-sections -fno-stack-protector \
-    -Wl,-O3,--lto-O3,--gc-sections,--as-needed,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr,-x,-s \
-    $(pwd)/*.cpp -o $(dirname "$0")/FeasArgSet && echo "*编译完成*" || exit 1
-    #-fPIC -llog \
-    /data/data/com.termux/files/usr/bin/aarch64-linux-android-strip $(dirname "$0")/FeasArgSet
-    #/data/data/com.termux/files/usr/bin/upx -9 $(dirname "$0")/FeasArgSet
-    chmod +x $(dirname "$0")/FeasArgSet
-    cp -f $(dirname "$0")/FeasArgSet  ../Module/FeasArgSet
-    mv -f $(dirname "$0")/FeasArgSet  ../Test/FeasArgSet
-    echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
+compile_start() {
+	echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
+	echo "开始编译，大概10秒完成"
+	cd $(dirname "$0")/../src
+	#this common from user : shadow3aaa
+	aarch64-linux-android-clang++ \
+		-Wall -std=c++2b -stdlib=libc++ \
+		-fno-rtti -fvisibility=hidden -static-libstdc++ \
+		-fshort-enums -fmerge-all-constants -fno-exceptions -fuse-ld=lld \
+		-Bsymbolic -fdata-sections -ffunction-sections -fno-stack-protector \
+		-Wl,-O3,--lto-O3,-flto,--gc-sections,--as-needed,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr,-x,-s \
+		$(pwd)/*.cpp -o $(dirname "$0")/FeasArgSet && echo "*编译完成*" || exit 1
+	#-fPIC -llog \
+	/data/data/com.termux/files/usr/bin/aarch64-linux-android-strip $(dirname "$0")/FeasArgSet
+	#/data/data/com.termux/files/usr/bin/upx -9 $(dirname "$0")/FeasArgSet
+	chmod +x $(dirname "$0")/FeasArgSet
+	cp -f $(dirname "$0")/FeasArgSet ../Module/FeasArgSet
+	mv -f $(dirname "$0")/FeasArgSet ../Test/FeasArgSet
+	echo "当前时间：$(date +%Y) 年 $(date +%m) 月 $(date +%d) 日 $(date +%H) 时 $(date +%M) 分 $(date +%S) 秒"
 }
 
 format_code
